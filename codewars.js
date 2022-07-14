7d60cdb519e1a8ac863b72fa043e713eb1cf5e5e
Gjarred Baker <gjarred23@gmail.com>
2022-07-13 22:17:11 -0500

    greet input phrase

--- codewars.js
+++ codewars.js
@@ -16,3 +16,13 @@ function numberToString(num) {
   return num.toString();
   
 }
+
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+  //your code here
+  const phrase = `Hello, ${name} how are you doing today?`;
+  return phrase
+}
