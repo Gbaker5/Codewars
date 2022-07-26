5dc05f3c0bf7c980c1c6b7fe83c09d43e6077fde
Gjarred Baker <gjarred23@gmail.com>
2022-07-25 22:26:21 -0500

    countSheeps

--- codewars.js
+++ codewars.js
@@ -100,3 +100,24 @@ function find_average(array) {
     }   
  
 }
+
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+//For example,
+
+//[true,  true,  true,  false,
+  //true,  true,  true,  true ,
+  //true,  false, true,  false,
+  //true,  false, false, true ,
+  //true,  true,  true,  true ,
+  //false, false, true,  true]
+//The correct answer would be 17.
+
+//Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+  
+  trueFilter = arrayOfSheep.filter(word => word === true )//filters all true from array into new array
+  
+  return(trueFilter.length)//arr.length counts the number of items in that array
+}
