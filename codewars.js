abb32cc1aa94f3e6d3d3aba9cdcf94ef508bfde0
Gjarred Baker <gjarred23@gmail.com>
2022-08-03 00:08:00 -0500

    square root

--- codewars.js
+++ codewars.js
@@ -158,3 +158,23 @@ function removeExclamationMarks(s) {
   let removeIt = s.replace(/\!/g, '')
   return removeIt;
 }
+
+//Given an integral number, determine if it's a square number:
+
+
+//Examples
+
+//-1  =>  false
+ //0  =>  true
+ //3  =>  false
+ //4  =>  true
+//25  =>  true
+//26  =>  false
+
+var isSquare = function(n){
+  if(Math.sqrt(n) === Math.round(Math.sqrt(n))){
+    return true
+  }else{
+    return false
+  }
+}
