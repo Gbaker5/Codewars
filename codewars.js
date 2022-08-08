0bd3af86eab85b2264011bf63eb4cf9430cab2b3
Gjarred Baker <gjarred23@gmail.com>
2022-08-08 10:57:08 -0500

    positivenums

--- codewars.js
+++ codewars.js
@@ -178,3 +178,20 @@ var isSquare = function(n){
     return false
   }
 }
+
+//You get an array of numbers, return the sum of all of the positives ones.
+
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let positiveNums = arr.filter(nums => nums > 0)
+  
+  let sum = 0;
+
+  for(let i = 0; i < positiveNums.length; i++){
+      sum += positiveNums[i]
+  }
+   return sum
+}     
