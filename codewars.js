bc5687059fe2539b649092261e7c5ff088010161
Gjarred Baker <gjarred23@gmail.com>
2022-08-14 15:28:51 -0500

    booleantostring

--- codewars.js
+++ codewars.js
@@ -219,3 +219,20 @@ function filter_list(l) {
 function solution(str, ending){
   return str.endsWith(ending)
 }
+
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i*2;
+}
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+function booleanToString(b){
+  if(b === true){
+    return "true";
+    }else return "false"
+}
