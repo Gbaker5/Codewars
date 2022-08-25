d925daa595a8e38496fa7d9ec2559ac013cdec37
Gjarred Baker <gjarred23@gmail.com>
2022-08-24 21:50:46 -0500

    XO

--- codewars.js
+++ codewars.js
@@ -369,3 +369,27 @@ function litres(time) {
   return Math.floor(time*0.5)
 }
 
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+//Examples input/output:
+
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
+
+function XO(str) {
+  let string = str.toLowerCase()
+   
+   let array = Array.from(string)
+   
+   let exs = array.filter( letter => letter == ('x'))
+   
+   let ohs = array.filter( letter => letter == ('o'))
+   
+   if (exs.length == ohs.length){
+       return true
+   }else return false
+
+}
