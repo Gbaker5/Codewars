776e25297709493f3c1a970edd20ec0b73407eb4
Gjarred Baker <gjarred23@gmail.com>
2022-08-25 23:01:55 -0500

    sum

--- codewars.js
+++ codewars.js
@@ -440,3 +440,15 @@ const areaOrPerimeter = function(l , w) {
     return l * w
   }else return ( l*2) + (w*2)
  };
+
+ //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+ function sum (numbers) {
+  let sum = 0;
+ for(let i=0; i< numbers.length; i++){
+  sum += numbers[i]
+ }
+ if(numbers== ""){
+   return(0)
+ }else return(sum)
+}
