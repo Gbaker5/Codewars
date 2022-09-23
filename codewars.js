d09a6a8ee05c8cb86feda9dfd16bc5ec4809fe1c
Gjarred Baker <gjarred23@gmail.com>
2022-09-23 11:55:23 -0500

    accum

--- codewars.js
+++ codewars.js
@@ -489,3 +489,22 @@ function openOrSenior(data){
   }
   return data.map(determineMembership);
 }
+
+//This time no story, no theory. The examples below show you how to write function accum:
+
+//Examples:
+
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  var letters = s.split(''), words = [];
+
+  for(var i = 0; i < letters.length; i++) {
+    words.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+
+  return words.join('-');
+}
