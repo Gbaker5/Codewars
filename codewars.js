4fdd7112b750b30348137ce54f26f8b58f65bc8a
Gjarred Baker <gjarred23@gmail.com>
2022-09-25 18:34:15 -0500

    dna

--- codewars.js
+++ codewars.js
@@ -539,3 +539,14 @@ function likes(names) {
   }
   
   likes(['Peter', 'Alex', 'Mark', 'Max', 'Sarah'])
+
+//Create a function which translates a given DNA string into RNA.
+
+//For example:
+
+//"GCAT"  =>  "GCAU"
+//The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g,"U")
+  }
