f89e170fad9a4eec6e6498692cfcd216de5ede35
Gjarred Baker <gjarred23@gmail.com>
2022-12-08 18:22:40 -0600

    alpabet position

--- codewars.js
+++ codewars.js
@@ -580,3 +580,29 @@ function boolToWord( bool ){
     return 'Yes'
   }else return 'No'
 }
+
+//In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+//If anything in the text isn't a letter, ignore it and don't return it.
+
+//"a" = 1, "b" = 2, etc.
+
+//Example
+
+//alphabetPosition("The sunset sets at twelve o' clock.")
+//Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+alphabetPosition("the sunset sets at twelve o' clock.")
+
+function alphabetPosition(text) {
+    var result = "";
+    for (var i = 0; i < text.length; i++) {
+      var code = text.toUpperCase().charCodeAt(i)
+      console.log(code)
+      if (code > 64 && code < 91) result += (code - 64) + " ";
+    }
+  
+    return result.slice(0, result.length - 1);
+  }
+  console.log(alphabetPosition("The sunset sets at twelve o' clock."));
