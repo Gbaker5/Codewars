5209110f4936e319165be37d4249bafba2058c14
Gjarred Baker <gjarred23@gmail.com>
2023-01-08 15:28:40 -0600

    findNextSquare

--- codewars.js
+++ codewars.js
@@ -646,3 +646,23 @@ function arrayDiff(a, b) {
   }
 
   }
+
+  //Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+  //If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+  function findNextSquare(sq) {
+    let result = Math.sqrt(sq)
+    console.log(result)
+    
+    if(result % 1 != 0){
+      console.log('Not Squared Num')
+      return -1
+    }else{
+      console.log('Squared Num')
+      let plusOne = (result + 1)
+      console.log(plusOne)
+      let nextSquare = plusOne * plusOne
+      console.log(nextSquare)
+    }
+  }
