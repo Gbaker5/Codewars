fc96d818164be8f9dcac214539cddd0166b45341
Gjarred Baker <gjarred23@gmail.com>
2023-01-08 16:20:05 -0600

    powers of two

--- codewars.js
+++ codewars.js
@@ -666,3 +666,25 @@ function arrayDiff(a, b) {
       console.log(nextSquare)
     }
   }
+
+
+  //Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+//Examples
+
+//n = 0  ==> [1]        # [2^0]
+//n = 1  ==> [1, 2]     # [2^0, 2^1]
+//n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+function powersOfTwo(n){
+  result = []; //result is an empty array
+  for(let i = 0; i <= n; i++ ){ //for loop that stops at variable given (n)
+    let powers = Math.pow(2,(i)) //variable that holds result of looping through each iteration of 2 to the (n) power
+    result.push(powers) //push each value of powers into array
+  }
+
+  return result
+}
+
+powersOfTwo(4)
+  
