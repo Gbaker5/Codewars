6db0c49b7ca408eab8492a8f66484698b9976b02
Gjarred Baker <gjarred23@gmail.com>
2023-01-09 08:05:10 -0600

    playing banjo

--- codewars.js
+++ codewars.js
@@ -758,3 +758,23 @@ function strCount(str, letter){
     console.log(count)
   }
 }
+
+//Create a function which answers the question "Are you playing banjo?".
+//If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+//The function takes a name as its only argument, and returns one of the following strings:
+
+//name + " plays banjo" 
+//name + " does not play banjo"
+//Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  let letter = name[0].toLowerCase()
+  console.log(letter)
+  if(letter == "r"){
+    console.log(name + " plays banjo")
+  }else{
+    console.log(name + " does not play banjo")
+  }
+  
+}
