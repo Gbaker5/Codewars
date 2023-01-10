7527d55895541088c617bf515643bef10a09d3eb
Gjarred Baker <gjarred23@gmail.com>
2023-01-10 11:14:45 -0600

    howMuchILoveYou

--- codewars.js
+++ codewars.js
@@ -687,4 +687,34 @@ function powersOfTwo(n){
 }
 
 powersOfTwo(4)
+
+//Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+//"I love you"
+//"a little"
+//"a lot"
+//"passionately"
+//"madly"
+//"not at all"
+//If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+//When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+//Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+
+function howMuchILoveYou(nbPetals) {
+  let petal = ["I love You", //variable holding array that holds the 6 choices
+  "a little",
+  "a lot",
+  "passionately",
+  "madly",
+  "not at all"]
   
+  let choice = []; 
+
+  for(let i = 0; i<nbPetals; i++){ //for loop that terates as many times as the value of nbPetals
+    choice = ( petal[i%6]) //variable holds choice after iterating through the array it will loop back through the same array. (could also be written (petal[i%petal.length]) to express the repeating point in the array)
+  }
+  console.log(choice)
+}
