4043a00d85e1b75718461fd88c498e826030c98a
Gjarred Baker <gjarred23@gmail.com>
2023-01-10 12:23:33 -0600

    strCount

--- codewars.js
+++ codewars.js
@@ -736,3 +736,25 @@ function updateLight(current) {
   //your code here!
 
 }
+
+//Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+//If no occurrences can be found, a count of 0 should be returned.
+
+//("Hello", "o")  ==>  1
+//("Hello", "l")  ==>  2
+//("", "z")       ==>  0
+
+function strCount(str, letter){  
+  let count = 0;
+  let arr = Array.from(str)
+  let letterArr = Array.from(letter)
+  console.log(arr)
+  console.log(letterArr)
+  
+  for(i=0; i<arr.length; i++){
+    if(arr[i] == letterArr)
+    count++;
+    console.log(count)
+  }
+}
