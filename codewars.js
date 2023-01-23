9fad73625bd38537e74f57bc898349792d6567ed
Gjarred Baker <gjarred23@gmail.com>
2023-01-23 10:05:33 -0600

    goosefilter

--- codewars.js
+++ codewars.js
@@ -979,3 +979,16 @@ console.log( languagePairs['english'])
 
 }
 greet('dutch')
+
+//Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+//The geese are any strings in the following array, which is pre-populated in your solution:
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  filtered = birds.filter(word => !geese.includes(word))
+  console.log(filtered)
+  // return an array containing all of the strings in the input array except those that match strings in geese
+};
+
+gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])
