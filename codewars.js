f9b7dd32880cca18acb2585bcf209aeac3ec655c
Gjarred Baker <gjarred23@gmail.com>
2023-01-23 13:00:27 -0600

    binaryArraytoNUmber

--- codewars.js
+++ codewars.js
@@ -1030,3 +1030,39 @@ function finalGrade( exam, projects){
 }
 
 finalGrade(55, 0)
+
+
+//Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+//Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+//Examples:
+
+//Testing: [0, 0, 0, 1] ==> 1
+//Testing: [0, 0, 1, 0] ==> 2
+//Testing: [0, 1, 0, 1] ==> 5
+//Testing: [1, 0, 0, 1] ==> 9
+//Testing: [0, 0, 1, 0] ==> 2
+//Testing: [0, 1, 1, 0] ==> 6
+//Testing: [1, 1, 1, 1] ==> 15
+//Testing: [1, 0, 1, 1] ==> 11
+//However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = arr => {
+  reversedArray = arr.reverse() //reversed the original array
+  console.log(reversedArray)
+  
+  binaryNum = [] ;
+  sum = 0;
+
+  for(i=0;i<reversedArray.length;i++){
+    binaryNum.push(reversedArray[i] * Math.pow(2,[i])) //iterated through each number of array at given index and multiples by exponents of 2 at the same index
+    console.log(binaryNum)
+    //console.log([reversedArray[i] * Math.pow(2,[i])]
+  }
+  sum = binaryNum.reduce((x,y) => x + y) //add each multiple together
+  
+  console.log(sum)
+};
+
+binaryArrayToNumber([1,0,1,1,1])
