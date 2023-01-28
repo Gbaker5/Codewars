d4ac6d1ea84ab19d5cdc321ca4a6ec78e236c883
Gjarred Baker <gjarred23@gmail.com>
2023-01-28 15:54:40 -0600

    invert

--- codewars.js
+++ codewars.js
@@ -1163,3 +1163,21 @@ var summation = function (num) {
 }
 
 summation(8)
+
+
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+//invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+//invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+//invert([]) == []
+//You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  opposites = [];
+  for(i=0;i<array.length;i++){
+    opposites.push(array[i] * -1)
+  }
+  console.log(opposites)
+}
+
+invert([1,-2,3,-4,5])
