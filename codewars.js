64cf4a95c739cd047b782fdb933c0c1f3b70bb3a
Gjarred Baker <gjarred23@gmail.com>
2023-01-28 16:19:21 -0600

    quarterOf

--- codewars.js
+++ codewars.js
@@ -1181,3 +1181,45 @@ function invert(array) {
 }
 
 invert([1,-2,3,-4,5])
+
+//Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+//For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+//Constraint:
+
+//1 <= month <= 12
+
+const quarterOf = (month) => {
+  let quarter = 0;
+  
+  for(i=1;i<=3;i++){
+    
+    if(month === i){
+      quarter = 1
+    }
+  }
+  
+  for(i=4;i<=6;i++){
+    
+    if(month === i){
+      quarter = 2
+    }
+  }
+  
+  for(i=7;i<=9;i++){
+    
+    if(month === i){
+      quarter = 3
+    }
+  }
+  
+  for(i=10;i<=12;i++){
+    
+    if(month === i){
+      quarter = 4
+    }
+  }
+    console.log(quarter)
+  }
+  quarterOf(10)
