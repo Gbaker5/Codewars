31b75bf7ecbd263420a373828a5fec0754c511c2
Gjarred Baker <gjarred23@gmail.com>
2023-01-28 15:33:13 -0600

    summation

--- codewars.js
+++ codewars.js
@@ -1143,3 +1143,23 @@ function countSmileys(arr) {
 function square(num){
   return Math.pow(num, 2)
 }
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+//For example (Input -> Output):
+
+//2 -> 3 (1 + 2)
+//8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+var summation = function (num) {
+  arr= [];
+
+  for(i=1; i<=num;i++){
+    arr.push(i)
+  }
+  console.log(arr)
+  sum = arr.reduce((acc, initialValue) => acc + initialValue)
+  console.log(sum)
+}
+
+summation(8)
