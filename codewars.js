e60e0cb41fea5cd6199eda95c8ce76d3f18bb52e
Gjarred Baker <gjarred23@gmail.com>
2023-01-29 11:11:29 -0600

    fakebinary

--- codewars.js
+++ codewars.js
@@ -1251,3 +1251,32 @@ function rentalCarCost(d) {
 }
 
 rentalCarCost(8)
+
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+//Note: input will never be an empty string
+
+
+function fakeBin(x){
+  let arr = x.split('')
+  console.log(arr)
+  let nums = arr.map(n => Number(n))
+  console.log(nums)
+  
+  let newArray = [];
+  
+  for(i=0;i<nums.length;i++){
+  if(nums[i]<5){
+    newArray.push(0)
+  }else if(nums[i]>=5){
+    newArray.push(1)
+  }
+  
+  console.log(newArray)
+  
+  }
+  console.log(newArray.join(''))
+  
+  }
+  fakeBin('45385593107843568')
