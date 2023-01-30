07ac6097093711ff3ec5832de0c8480ea4f43eb6
Gjarred Baker <gjarred23@gmail.com>
2023-01-30 16:50:32 -0600

    solution reverse

--- codewars.js
+++ codewars.js
@@ -1350,3 +1350,13 @@ function sumStr(a,b) {
   }
   
   sumStr("4","5")
+
+  //Complete the solution so that it reverses the string passed into it.
+
+  //'world'  =>  'dlrow'
+  //'word'   =>  'drow'
+
+
+  function solution(str){
+    return str.split('').reverse().join('')
+  }
