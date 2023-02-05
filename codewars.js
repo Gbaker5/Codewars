13077377507a362b2575e0b019fe96c39d1220f0
Gjarred Baker <gjarred23@gmail.com>
2023-02-05 17:44:59 -0600

    jaden case

--- codewars.js
+++ codewars.js
@@ -1627,3 +1627,15 @@ function points(games) {
 points(["1:4","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:4","4:3"])
 
 
+//Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+//Example:
+
+//Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+//Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () { 
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
