237c3b63e05b862833a149d2db0dea290b8cb750
Gjarred Baker <gjarred23@gmail.com>
2023-02-05 17:44:07 -0600

    points

--- codewars.js
+++ codewars.js
@@ -1585,3 +1585,45 @@ function queueTime(customers, n) {
 }
 
 queueTime([5,3,4], 1)
+
+
+//Our football team has finished the championship.
+
+//Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+//For example: ["3:1", "2:2", "0:1", ...]
+
+//Points are awarded for each match as follows:
+
+//if x > y: 3 points (win)
+//if x < y: 0 points (loss)
+//if x = y: 1 point (tie)
+//We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+//Notes:
+
+//our team always plays 10 matches in the championship
+//0 <= x <= 4
+//0 <= y <= 4
+
+function points(games) {
+  let teamPoints = 0;
+
+  for(i=0;i<games.length;i++){
+   if(Number(games[i][0])>Number(games[i][2])){
+    console.log('win')
+    teamPoints+=3
+   }else if(Number(games[i][0])<Number(games[i][2])){
+    console.log('lose')
+   }else if (Number(games[i][0])==Number(games[i][2])){
+    console.log('tie')
+    teamPoints+=1
+   }
+   
+  }
+  console.log(teamPoints)
+}
+
+points(["1:4","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:4","4:3"])
+
+
