ef98acd9c7828ae15dbed05ff415f55c57e7a3db
Gjarred Baker <gjarred23@gmail.com>
2023-06-13 14:10:45 -0500

    descending order

--- codewars.js
+++ codewars.js
@@ -1657,3 +1657,26 @@ function paperwork(n, m) {
     return 0
   }else return n*m
 }
+
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+//Examples:
+
+//Input: 42145 Output: 54421
+
+//Input: 145263 Output: 654321
+
+//Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+  let arr = n.toString().split('')
+  console.log(arr)
+  numArr = arr.map(str => (str * 1)) //conversts string to num
+  console.log(numArr)
+  sort = numArr.sort((a,b) => b - a) // arrow function required for sort function a-b is ascending
+  console.log(sort)
+  let newNum = sort.join('') //return to string
+  console.log(parseInt(newNum)) //return to num
+}
+
+descendingOrder(42145)
