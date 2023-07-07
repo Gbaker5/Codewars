1a93054c699efac58fd76be7fab87c95108e994c
Gjarred Baker <gjarred23@gmail.com>
2023-07-06 21:32:42 -0500

    unusual five

--- codewars.js
+++ codewars.js
@@ -1859,3 +1859,14 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
       return false
   }
 };
+
+//Write a function that always returns 5
+
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+//Good luck :)
+
+function unusualFive() {
+  let five = 'Five!';
+  return five.length;
+  }
