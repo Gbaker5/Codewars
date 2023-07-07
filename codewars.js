0d65da56bf5ff00d4c6c0bc176d5b706f5f24ca2
Gjarred Baker <gjarred23@gmail.com>
2023-07-06 21:00:43 -0500

    countpositivessumnegatives

--- codewars.js
+++ codewars.js
@@ -1818,3 +1818,29 @@ function getMiddle(s){
 
 
 getMiddle("test")
+
+
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+//If the input is an empty array or is null, return an empty array.
+
+//Example
+
+//For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length < 1){
+      return []
+  }else{
+      let positivearr = input.filter(input => input > 0)
+      let negativearr = input.filter(input => input < 0)
+      let positivecount = positivearr.length
+      let negativesum = negativearr.reduce((acc, cur) => acc + cur, 0)
+      console.log([positivecount,negativesum])
+  }
+  
+  
+    
+}
+
+countPositivesSumNegatives([null])
