f8c0d9a474b8f6ad1d8ac0a2b33166a015ac3c95
Gjarred Baker <gjarred23@gmail.com>
2023-07-11 20:58:56 -0500

    getvolume

--- codewars.js
+++ codewars.js
@@ -1949,3 +1949,11 @@ function minMax(arr){
   return [min,max]
   
 }
+
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length*width*height
+  }
+}
