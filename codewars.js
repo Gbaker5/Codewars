ba17d23d7b6868523e883ce69ad4fc07ad64c798
Gjarred Baker <gjarred23@gmail.com>
2023-07-11 21:49:55 -0500

    xor

--- codewars.js
+++ codewars.js
@@ -1989,3 +1989,14 @@ function calculateYears(principal, interest, tax, desired) {
 
 
 calculateYears(1000,0.05,0.18,1100)
+
+
+//Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+
+function xor(a, b) {
+  if(a == b){
+    return false
+  }else return true
+  // TODO: Program Me
+}
