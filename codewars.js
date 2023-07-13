f217538ef5703a105596bc029c3f7b39dfbfc3c2
Gjarred Baker <gjarred23@gmail.com>
2023-07-12 21:06:45 -0500

    wave

--- codewars.js
+++ codewars.js
@@ -2047,3 +2047,24 @@ function switchItUp(number){
     }
       
   }
+
+
+  //In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+//Rules
+
+ //1.  The input string will always be lower case but maybe empty.
+
+ //2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+//Example
+
+//wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str){
+  // Code here
+  arr= str.split("")
+  for(i=0;i<arr.length;i++){
+     console.log(str.substring(0,[i]) + arr[i].toUpperCase() + str.substring([i+1])) 
+  }
+}
+
+wave("hello")
