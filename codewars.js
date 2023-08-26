7da3ceb3678487a876ad72358a97ffc1177e91b8
Gjarred Baker <gjarred23@gmail.com>
2023-08-26 10:41:17 -0500

    areacode

--- codewars.js
+++ codewars.js
@@ -2092,3 +2092,23 @@ function count(string) {
 }
 
 count('aba')
+
+//You've got a bunch of textual data with embedded phone numbers. Write a function area_code() that finds and returns just the area code portion of the phone number.
+
+//>>> message = "The supplier's phone number is (555) 867-5309"
+//>>> area_code(message)
+//'555'
+//The returned area code should be a string, not a number. Every phone number is formatted like in the example, and the only non-alphanumeric characters in the string are apostrophes ' or the punctuation used in the phone number.
+
+
+function areaCode(text) {
+  arr= text.split('')
+  index = arr.indexOf('(')
+  console.log(index)
+
+  areaC = text.substring(index+1, index+4 )
+  console.log(areaC)
+  
+}
+
+areaCode("The supplier's phone number is (555) 867-5309")
