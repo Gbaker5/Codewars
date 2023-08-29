d29c4ff4f6aca114400e719c30134777e3916bc2
Gjarred Baker <gjarred23@gmail.com>
2023-08-28 22:11:13 -0500

    to snake_case

--- codewars.js
+++ codewars.js
@@ -2153,3 +2153,21 @@ function quotable(name, quote){
  }
 
  quotable('Grae', 'Practice makes perfect')
+
+
+ //Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
+
+//Examples
+
+//"TestController"  -->  "test_controller"
+//"MoviesAndBooks"  -->  "movies_and_books"
+//"App7Test"        -->  "app7_test"
+//1                 -->  "1"
+
+function toUnderscore(string) {
+  console.log(string)
+  arr = string.split(/\.?(?=[A-Z])/g).join('_')
+  console.log(arr)
+}
+
+toUnderscore("TestController7Champ")
