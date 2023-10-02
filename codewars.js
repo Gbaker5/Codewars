31aeccc85b333cb72056bfc50823f59340f564b7
Gjarred Baker <gjarred23@gmail.com>
2023-10-02 08:00:00 -0500

    greet

--- codewars.js
+++ codewars.js
@@ -2335,3 +2335,18 @@ function noBoringZeros(n) {
 }
 
 noBoringZeros(-10500000)
+
+
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:
+
+//case	return
+//name equals owner	'Hello boss'
+//otherwise	'Hello guest'
+
+function greet (name, owner) {
+  if(name == owner){
+      return 'Hello boss'
+  }else return 'Hello guest'
+}
