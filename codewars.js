2433c9df9823b91d049c93228259ef7fff6c21ae
Gjarred Baker <gjarred23@gmail.com>
2023-10-07 16:24:07 -0500

    take

--- codewars.js
+++ codewars.js
@@ -2445,3 +2445,13 @@ function solve(s){
 }
 
 solve('code')
+
+
+//Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(arr, n) {
+  console.log(arr.slice(0,n))
+  
+}
+
+take([0,1,2,3,5,8,13],3)
