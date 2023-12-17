5de396127ec557dc10f1396ee776a7b150cf6e3b
Gjarred Baker <gjarred23@gmail.com>
2023-12-17 15:57:40 -0600

    distinct

--- codewars.js
+++ codewars.js
@@ -2497,3 +2497,21 @@ function isPangram(sentence){
 }
 
 isPangram("The quick brown fox jumps over the lazy dog")
+
+//Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+//The order of the sequence has to stay the same.
+
+//Examples:
+
+//Input -> Output
+//[1, 1, 2] -> [1, 2]
+//[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+  console.log(a)
+  const noDuplicates = a.filter((item, index) => a.indexOf(item) == index)
+  console.log(noDuplicates)
+}
+
+distinct([1, 2, 1, 1, 3, 2])
