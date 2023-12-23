aef5eea18a721d28bda577ee718de74c32a19fe3
Gjarred Baker <gjarred23@gmail.com>
2023-12-23 00:30:47 -0600

    removeurlanchor

--- codewars.js
+++ codewars.js
@@ -2705,3 +2705,19 @@ function combat(health, damage) {
     }
     else return health - damage
   }
+
+  //Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+//Examples
+
+//"www.codewars.com#about" --> "www.codewars.com"
+//"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+  const pound = url.indexOf("#")
+  console.log(pound)
+  if(pound == -1){
+      return url
+  }else return url.slice(0, pound)
+  
+}
