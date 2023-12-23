9d4a909a71cb3530879677b631c4c4869585e866
Gjarred Baker <gjarred23@gmail.com>
2023-12-23 00:19:53 -0600

    combat damage

--- codewars.js
+++ codewars.js
@@ -2696,3 +2696,12 @@ function alphabetPosition(text) {
 }
 
 alphabetPosition("The sunset sets at twelve o' clock.")
+
+//Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  if (health - damage < 0){
+    return +0
+    }
+    else return health - damage
+  }
