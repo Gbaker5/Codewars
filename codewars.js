6f6fc5df27bed2896d231b5426357a9573818390
Gjarred Baker <gjarred23@gmail.com>
2024-01-01 17:51:33 -0600

    validatepin

--- codewars.js
+++ codewars.js
@@ -2802,3 +2802,37 @@ function cockroachSpeed(s) {
 }
 
 expandedForm(70304)
+
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+//If the function is passed a valid PIN string, return true, else return false.
+
+//Examples (Input --> Output)
+
+//"1234"   -->  true
+//"12345"  -->  false
+//"a234"   -->  false
+
+function validatePIN (pin) {
+  console.log(pin)
+
+  const arr = pin.split("") //pin string arr
+  console.log(arr)
+  if( arr == "" ){
+      return false
+  }
+  const regex = pin.match(/([0-9])/g).join("") //regex check for all nums
+  console.log(regex)
+
+  
+  if(arr.length == 4 & pin == regex || arr.length == 6 & pin == regex){
+      console.log(true)
+  }else console.log(false)
+
+
+
+
+
+}
+
+validatePIN("1234")
