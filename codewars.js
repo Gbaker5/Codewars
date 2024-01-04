07b5075f09869df69515017e0a901d23486f3ec0
Gjarred Baker <gjarred23@gmail.com>
2024-01-04 15:55:59 -0600

    stocklist

--- codewars.js
+++ codewars.js
@@ -2937,3 +2937,67 @@ String.prototype.toAlternatingCase = function () {
   return newArr.join("")
 }
 
+//A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+
+//In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+//For example an extract of a stocklist could be:
+
+//L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+//or
+//L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+//You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+//M = {"A", "B", "C", "W"} 
+//or
+//M = ["A", "B", "C", "W"] or ...
+//and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+//For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket/Prolog a list of pairs):
+
+//(A : 20) - (B : 114) - (C : 50) - (W : 0)
+//where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 114 the sum corresponding to "BKWRK" and "BTSQZ", 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+//If L or M are empty return string is "" (Clojure/Racket/Prolog should return an empty array/list instead).
+
+
+
+function stockList(listOfArt, listOfCat){
+
+  console.log(listOfArt[0].split(" "))
+  
+  // Initialize a dictionary to store the sum for each category
+  let categorySums = {};
+  listOfCat.forEach(category => {
+      categorySums[category] = 0;
+  });
+
+  console.log(categorySums)
+
+   // Iterate through each book in the stocklist
+   listOfArt.forEach(book => {
+      let [code, quantity] = book.split(" "); //["BBAR", "150"] ["CDXE","515"]
+      let category = code[0]; //"B" "C"
+
+      // Check if the category is in the list of categories
+      if (listOfCat.includes(category)) { 
+          categorySums[category] += parseInt(quantity, 10);
+      }
+  });
+  
+  console.log(categorySums)
+  
+   // Build the result string
+   let result = listOfCat.map(category => `(${category} : ${categorySums[category] || 0})`).join(" - ");
+
+    ;
+  console.log(result)
+  //const = 
+  //console.log()
+  //const = 
+  //console.log()
+}
+
+stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"],["A", "B", "C", "D"])
+
+
