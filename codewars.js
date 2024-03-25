38edd09379197e318b1d0c13c0a4b7b1fe4e81ee
Gjarred Baker <gjarred23@gmail.com>
2024-03-24 22:01:38 -0500

    find average

--- codewars.js
+++ codewars.js
@@ -3001,3 +3001,69 @@ function stockList(listOfArt, listOfCat){
 stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"],["A", "B", "C", "D"])
 
 
+//every/little/things/is/gonna/be/alright/doo/dooo/da/doooo
+
+//We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+//You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+//All values will be positive integers or floats, or zero.
+
+
+function plural(n) {
+  if(n == 1){
+    return false
+  }else return true
+ }//
+
+
+ //Find Mean
+
+//Find the mean (average) of a list of numbers in an array.
+//
+//Information
+//
+//To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+//
+//For an example list of 1, 3, 5, 7
+//
+//1. Add all of the numbers
+//
+//1+3+5+7 = 16
+//2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+//
+//16/4 = 4
+//3. The mean (or average) of this list is 4
+//
+
+
+var findAverage = function (nums) {
+
+  let sum = nums.reduce((acc,c) => acc + c)
+  
+  let count = nums.length
+
+
+  return sum/count
+
+}
+
+
+
+//Task
+//
+//Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+//
+//The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+//
+//Mind the input validation.
+//
+//Example
+//
+//{ 6, 2, 1, 8, 10 } => 16
+//{ 1, 1, 11, 2, 3 } => 6
+//Input validation
+//
+//If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+
