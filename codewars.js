edbed8fadee067496ed56b1863f9144317187f55
Gjarred Baker <gjarred23@gmail.com>
2024-03-28 21:45:10 -0500

    Lightsabers

--- codewars.js
+++ codewars.js
@@ -3121,3 +3121,21 @@ function rot13(message){
 }
 rot13("Test two!") //test = grfg
 
+//Inspired by the development team at Vooza, write the function that
+//
+//accepts the name of a programmer, and
+//returns the number of lightsabers owned by that person.
+//The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+//
+//Note: your function should have a default parameter.
+//
+//For example(Input --> Output):
+
+function howManyLightsabersDoYouOwn(name) {
+  if(name == "Zach"){
+     return 18
+  }else {
+    return 0
+  }
+ 
+}
