f5d770f8bdee704dab6d367916edce2510571b88
Gjarred Baker <gjarred23@gmail.com>
2024-03-31 12:52:25 -0500

    say Hello

--- codewars.js
+++ codewars.js
@@ -3139,3 +3139,24 @@ function howManyLightsabersDoYouOwn(name) {
   }
  
 }
+
+
+//Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+//
+//Example:
+//
+//['John', 'Smith'], 'Phoenix', 'Arizona'
+//This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+
+function sayHello( name, city, state ) {
+  
+  if(name.length > 0 ){
+     name = name.join(" ")
+     console.log(name)
+  }
+   
+ console.log(`Hello ${name}!, Welcome to ${city}, ${state}`)
+ }
+ 
+ sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
