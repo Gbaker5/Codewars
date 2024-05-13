7e8202a40677bfa84f37c256e7f78928e98bf59d
Gjarred Baker <gjarred23@gmail.com>
2024-05-13 15:37:00 -0500

    grasshopper

--- codewars.js
+++ codewars.js
@@ -3466,3 +3466,10 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   
   return diff
 }
+
+
+//A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main (verb, noun){
+  return verb + noun
+}
