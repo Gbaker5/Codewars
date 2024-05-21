27c4ec18dc480fa41bfa39747d87a3b9810e3d9d
Gjarred Baker <gjarred23@gmail.com>
2024-05-21 18:11:08 -0500

    numbered

--- codewars.js
+++ codewars.js
@@ -3513,3 +3513,27 @@ function towerBuilder(nFloors) {
 
 
 towerBuilder(4)
+
+//  Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+//
+//Write a function which takes a list of strings and returns each line prepended by the correct number.
+//
+//The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//
+//Examples: (Input --> Output)
+//
+//[] --> []
+//["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+    
+  let numbered = [];
+
+  for(i=0;i<array.length;i++){
+      numbered.push([i+1] + ":" + array[i])
+  }
+  console.log(numbered)
+  
+}
+
+number(["a", "b", "c"])
