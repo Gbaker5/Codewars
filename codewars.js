cf54ccf2a1d6b168dfeabfe00f03d993c026815b
Gjarred Baker <gjarred23@gmail.com>
2024-05-22 21:53:09 -0500

    basicop

--- codewars.js
+++ codewars.js
@@ -3528,20 +3528,26 @@ towerBuilder(4)
 
 
 
-//var number=function(array){
-//    
-//  let numbered = [];
-//
-//  for(i=0;i<array.length;i++){
-//      numbered.push([i+1] + ":" + array[i])
-//  }
-//  console.log(numbered)
-//  
-//}
-//
-//number(["a", "b", "c"])
-//
-//
+var number=function(array){
+    
+  let numbered = [];
+
+  for(i=0;i<array.length;i++){
+      numbered.push([i+1] + ":" + array[i])
+  }
+  console.log(numbered)
+  
+}
+
+number(["a", "b", "c"])
+
+
+
+
+
+
+
+
 //You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
 //
 //Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
@@ -3572,3 +3578,30 @@ if(lr == 0 && ud == 0 && direction.length == 10){
 }
 
 isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
+
+//Your task is to create a function that does four basic mathematical operations.
+//
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+//
+//Examples(Operator, value1, value2) --> output
+//
+//('+', 4, 7) --> 11
+//('-', 15, 18) --> -3
+//('*', 5, 5) --> 25
+//('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2){
+  let op = 0;
+
+  switch(operation){
+      case "+": op = parseInt(value1) + parseInt(value2); break;
+      case "-": op = parseInt(value1) - parseInt(value2); break;
+      case "*": op = parseInt(value1) * parseInt(value2); break;
+      case "/": op = parseInt(value1) / parseInt(value2); break;
+  }
+  
+  console.log(op)
+}
+
+basicOp("+", 4, 7)
