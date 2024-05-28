34258939cb1d0eddb511afda1bed0d3454cccb15
Gjarred Baker <gjarred23@gmail.com>
2024-05-28 08:52:40 -0500

    checkcoupon

--- codewars.js
+++ codewars.js
@@ -3625,3 +3625,14 @@ function binToDec(bin) {
   }
 
   binToDec("1001001")
+
+
+//  Your mission:
+//Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+//
+//A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    
+  return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
+  }
