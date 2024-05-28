dcfe16701182f0785c9796580f6c84b90c4697d6
Gjarred Baker <gjarred23@gmail.com>
2024-05-28 09:03:48 -0500

    remainder

--- codewars.js
+++ codewars.js
@@ -3636,3 +3636,13 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     
   return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
   }
+
+
+//  Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+//
+//Division by zero should return NaN.
+
+function remainder(n, m){
+  // Divide the larger argument by the smaller argument and return the remainder
+  return Math.max(n,m) % Math.min(n,m)
+}
