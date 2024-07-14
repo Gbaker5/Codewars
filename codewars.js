df598882ddd4dc5ec1dc31d73f2a1b8cd740fe5f
Gjarred Baker <gjarred23@gmail.com>
2024-07-13 21:17:41 -0500

    largest pair Sum

--- codewars.js
+++ codewars.js
@@ -3855,3 +3855,48 @@ function solution(nums){
  let sort = nums.sort((a,b) => a - b )
  return sort}
 }
+
+
+//Count the number of divisors of a positive integer n.
+//
+//Random tests go up to n = 500000.
+//
+//Examples (input --> output)
+//
+//4 --> 3 // we have 3 divisors - 1, 2 and 4
+//5 --> 2 // we have 2 divisors - 1 and 5
+//12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+//30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+
+function getDivisorsCnt(n){
+    
+
+  let divisors = [];
+
+ 
+ for(i=0;i<=n;i++){
+   
+   if(n == 0){
+       divisors.push(1)
+
+   }else if(n%i == 0){
+       divisors.push(i)
+   }
+}
+ 
+ return divisors.length
+} 
+
+//Given a sequence of numbers, find the largest pair sum in the sequence.
+//
+//For example
+//
+//[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+//[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+//Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum (numbers) {
+  let order = numbers.sort((a,b) => b - a)
+  let sum = order[0] + order[1]
+  return sum
+  }
