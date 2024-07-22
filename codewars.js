99d21bd08788fbd5814942a0a42a4e21d8819e20
Gjarred Baker <gjarred23@gmail.com>
2024-07-21 21:20:03 -0500

    whatday

--- codewars.js
+++ codewars.js
@@ -3980,3 +3980,45 @@ const stringToNumber = function(str){
   return parseInt(str)
    
  }
+
+//Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+//
+//The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+//
+//Upper or lower case letter does not matter -- "eNglisH" is also correct.
+//
+//Return value as boolean values, true for the string to contains "English", false for it does not.
+//
+
+function spEng(sentence){
+  const  lower = sentence.toLowerCase()
+ return lower.includes("english")
+ }
+
+// Complete the function which returns the weekday according to the input number:
+//
+//1 returns "Sunday"
+//2 returns "Monday"
+//3 returns "Tuesday"
+//4 returns "Wednesday"
+//5 returns "Thursday"
+//6 returns "Friday"
+//7 returns "Saturday"
+//Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+
+function whatday(num) { 
+
+  switch (num){
+    case 1: return "Sunday";
+    case 2: return "Monday";
+    case 3: return "Tuesday";
+    case 4: return "Wednesday";
+    case 5: return "Thursday";
+    case 6: return "Friday";
+    case 7: return "Saturday";
+    
+    default: return "Wrong, please enter a number between 1 and 7"
+      }
+  // put your code here
+    }
