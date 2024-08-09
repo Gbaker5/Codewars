5277505c2aa03de284eecfecb20a4ef85f07ae88
Gjarred Baker <gjarred23@gmail.com>
2024-08-08 21:33:17 -0500

    data reverse

--- codewars.js
+++ codewars.js
@@ -4284,3 +4284,39 @@ while(currentPercent > threshold){
 } 
 return days
 }
+
+//A stream of data is received and needs to be reversed.
+//
+//Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+//
+//11111111  00000000  00001111  10101010
+// (byte1)   (byte2)   (byte3)   (byte4)
+//should become:
+//
+//10101010  00001111  00000000  11111111
+// (byte4)   (byte3)   (byte2)   (byte1)
+//The total number of bits will always be a multiple of 8.
+//
+//The data is given in an array as such:
+//
+//[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+function dataReverse(data) {
+  const bytes = [];
+  
+  // Split the data array into chunks of 8 bits
+  for (let i = 0; i < data.length; i += 8) {
+      bytes.push(data.slice(i, i + 8));
+  }
+
+  cl(bytes)
+
+  // Reverse the array of chunks
+  bytes.reverse();
+  cl(bytes)
+  // Flatten the array of chunks back into a single array
+  const reversedData = bytes.flat();
+
+  cl(reversedData);
+}
+
