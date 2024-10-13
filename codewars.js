0f026d91dd0610622f4fb3ca6f3b253fd0cb4d47
Gjarred Baker <gjarred23@gmail.com>
2024-10-13 13:26:26 -0500

    getSum

--- codewars.js
+++ codewars.js
@@ -4320,3 +4320,33 @@ function dataReverse(data) {
   cl(reversedData);
 }
 
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+//Note: a and b are not ordered!
+//
+//Examples (a, b) --> output (explanation)
+//
+//(1, 0) --> 1 (1 + 0 = 1)
+//(1, 2) --> 3 (1 + 2 = 3)
+//(0, 1) --> 1 (0 + 1 = 1)
+//(1, 1) --> 1 (1 since both are same)
+//(-1, 0) --> -1 (-1 + 0 = -1)
+//(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+//Your function should only return a number, not the explanation about how you get that number.
+
+
+function getSum(a, b){
+  const numbers = [a,b]
+  const sorted = numbers.sort((a,b)=> a-b)
+  let fullSet = [];
+  if(a == b ){
+    return a
+ }else{
+  for(i=sorted[0];i<=sorted[1];i++){
+      fullSet.push(i)
+  }
+  const sum = fullSet.reduce((acc,c) => acc + c)
+  return sum
+ }
+
+}
