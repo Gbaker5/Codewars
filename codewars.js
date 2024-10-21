589ca40d60828642fd9511258c0230a831fa6b13
Gjarred Baker <gjarred23@gmail.com>
2024-10-21 12:33:53 -0500

    name shuffler

--- codewars.js
+++ codewars.js
@@ -4458,3 +4458,13 @@ function nbDig(n, d) {
 
 }
 
+//Write a function that returns a string in which firstname is swapped with last name.
+//
+//Example(Input --> Output)
+//
+//"john McClane" --> "McClane john"
+
+function nameShuffler(str){
+  const arr = str.split(" ")
+  return arr[1] + " " + arr[0]
+}
