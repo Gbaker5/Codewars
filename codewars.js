e643aab6498a7bf7586210b3ff76f49f7763973b
Gjarred Baker <gjarred23@gmail.com>
2024-12-29 18:54:36 -0600

    remove

--- codewars.js
+++ codewars.js
@@ -4881,3 +4881,19 @@ function thirt(n) {
 
 // Test the function
 console.log("Final Result:", thirt(1234567));
+
+
+//Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+//
+//Examples
+//
+//"Hi!"     ---> "Hi!"
+//"Hi!!!"   ---> "Hi!"
+//"!Hi"     ---> "Hi!"
+//"!Hi!"    ---> "Hi!"
+//"Hi! Hi!" ---> "Hi Hi!"
+//"Hi"      ---> "Hi!"
+
+function remove (string) {
+  return string.split("").filter((x) => x !== "!").join("") + "!"  
+  }
