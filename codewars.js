946e7fb8aa2f5add8ae89954a06dc32aa7c5381c
Gjarred Baker <gjarred23@gmail.com>
2025-01-05 21:21:29 -0600

    digits

--- codewars.js
+++ codewars.js
@@ -4939,4 +4939,15 @@ function remove (string) {
   };
   
   //solve("zodiac")
-  //solve("strength") 
+  //solve("strength")
+  
+
+  //Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+//
+//All inputs will be valid.
+
+function digits(n) {
+  cl(n.toString().split("").length)
+}
+
+digits(9876543210)
