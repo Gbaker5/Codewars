2c4ebe414f1cf16cecca3be3813be64f15dc72e1
Gjarred Baker <gjarred23@gmail.com>
2025-02-19 15:48:37 -0600

    getdivisorscnt

--- codewars.js
+++ codewars.js
@@ -5088,3 +5088,40 @@ function sumPairs(ints, s) {
 
   return undefined;
 }
+
+
+//This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+//
+//Add the value "codewars" to the array websites 1,000 times.
+
+let websites = [];
+  
+  for(i=0;i<1000;i++){
+    websites.push("codewars")
+  }
+  
+
+//  Count the number of divisors of a positive integer n.
+//
+//Random tests go up to n = 500000, but fixed tests go higher.
+//
+//Examples (input --> output)
+//
+//4 --> 3 // we have 3 divisors - 1, 2 and 4
+//5 --> 2 // we have 2 divisors - 1 and 5
+//12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+//30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+//Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+function getDivisorsCnt(n) {
+  let count =0;
+  
+  for(i=0;i*i<=n;i++){
+
+    if(n % i === 0 ){
+      count += (i * i === n) ? 1 : 2; 
+    }
+
+  }
+  return count
+}
