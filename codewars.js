1eef862598363fa7e4d27132d9843e4570c33413
Gjarred Baker <gjarred23@gmail.com>
2025-03-03 12:49:20 -0600

    cookie

--- codewars.js
+++ codewars.js
@@ -5142,6 +5142,16 @@ function findShort(s){
     return smallest
   }
 
+//Your task, is to create N×N multiplication table, of size provided in parameter.
+//
+//For example, when given size is 3:
+//
+//1 2 3
+//2 4 6
+//3 6 9
+//For the given example, the return value should be:
+//
+//[[1,2,3],[2,4,6],[3,6,9]]
 
   function multiplicationTable(size) {
     let arr = [];
@@ -5157,3 +5167,27 @@ function findShort(s){
   
   }
   
+
+//  For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+//
+//Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+//
+//Note: Make sure you return the correct message with correct spaces and punctuation.
+//
+//Please leave feedback for this kata. Cheers!
+
+function cookie(x){
+  //if(typeof x === "string"){
+  //  cl("Zach")
+  //}else if(typeof x === "number"){
+  //  cl("Monica")
+  //}else cl("the dog")
+
+  return typeof x === "string" ? "Who ate the last cookie? It was Zach!"
+  : typeof x === "number" ? "Who ate the last cookie? It was Monica!"
+  : "Who ate the last cookie? It was the dog!"
+}
+
+//cl(cookie(5))
+//cl(cookie("hey"))
+//cl(cookie(true))
