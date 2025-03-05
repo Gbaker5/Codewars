dffc49ebef7b542b6d6d6fc2b1fa0091a1b06bcb
Gjarred Baker <gjarred23@gmail.com>
2025-03-05 13:28:04 -0600

    shorten date

--- codewars.js
+++ codewars.js
@@ -5228,3 +5228,18 @@ function stray(numbers) {
 
 stray([17, 17, 3, 17, 17, 17, 17])
 stray([1,1,2])
+
+//Task Description
+//
+//You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+//
+//You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+//
+//Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+function shortenToDate(longDate) {
+  date = longDate.split(",")
+  date.pop()
+  cl(date.join(" "))
+ }
+ 
