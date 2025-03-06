bb52e89e0d7f8c43cb69f31097c2c8b929b8ee95
Gjarred Baker <gjarred23@gmail.com>
2025-03-06 10:26:39 -0600

    calculator

--- codewars.js
+++ codewars.js
@@ -5293,3 +5293,18 @@ checkForFactor(9,2)
 checkForFactor(653,7)
 checkForFactor(2453,5)
 checkForFactor(24617,3)
+
+//You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+//
+//Your function will accept three arguments:
+//The first and second argument should be numbers.
+//The third argument should represent a sign indicating the operation to perform on these two numbers.
+//
+//if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+//
+//Example:
+//
+//calculator(1,2,"+"); //=> result will be 3
+//calculator(1,2,"&"); //=> result will be "unknown value"
+//calculator(1,"k","*"); //=> result will be "unknown value"
+//Good luck!
