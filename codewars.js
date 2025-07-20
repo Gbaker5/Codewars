131458e284c700ecb207271f5425b82d62cc29d6
Gjarred Baker <gjarred23@gmail.com>
2025-07-19 19:48:57 -0500

    minelocation

--- codewars.js
+++ codewars.js
@@ -5554,3 +5554,35 @@ function decode(string) {
 
 encode("hello")
 decode("h2ll4")
+
+//You've just discovered a square (NxN) field and you notice a warning sign. The sign states that there's a single bomb in the 2D grid-like field in front of you.
+//
+//Write a function that accepts a 2D array, and returns the location of the mine. The mine is represented as the integer 1 in the 2D array. Areas in the 2D array that are not the mine will be represented as 0s.
+//
+//The location returned should be an array (Tuple<int, int> in C#, RAX:RDX in NASM) where the first element is the row index, and the second element is the column index of the bomb location (both should be 0 based). All 2D arrays passed into your function will be a square (NxN), and there will only be one mine in the array.
+//
+//Examples (Input --> Output)
+//
+//[ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] --> [0, 0]
+//
+//[ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] --> [1, 1]
+//
+//[ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] --> [2, 1]
+
+function mineLocation(field){
+  let arr = [] //create empty array
+  for(i=0;i<field.length; i++){
+    if(field[i].includes(1)){ //find index of array that has the mine
+      arr.push(i) //push that index to empty arr
+    }
+  }
+
+ let index = arr[0] //take that index
+ let fieldindex = field[index].indexOf(1) //use index to select the array that contains mine(1) then get the index of 1. 
+ 
+ arr.push(fieldindex) //push that index number to arr. return arr
+ return arr
+
+  
+    
+}
