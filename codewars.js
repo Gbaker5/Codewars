10c75f78f0d9eab9225a9413a4fe2eb9d2fe1313
Gjarred Baker <gjarred23@gmail.com>
2025-09-18 00:39:14 -0500

    alphabet wars

--- codewars.js
+++ codewars.js
@@ -5778,3 +5778,102 @@ function getRealFloor(n) {
     return n-1
   }else return n-2
 }
+
+//There is a war and nobody knows - the alphabet war!
+//There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+//
+//Task
+//
+//Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+//
+//The left side letters and their power:
+//
+// w - 4
+// p - 3
+// b - 2
+// s - 1
+//The right side letters and their power:
+//
+// m - 4
+// q - 3
+// d - 2
+// z - 1
+//The other letters don't have power and are only victims. Sum up each side's letters' power values to determine which side wins.
+//
+//Example
+//
+//alphabetWar("z");        //=> Right side wins!
+//alphabetWar("zdqmwpbs"); //=> Let's fight again!
+//alphabetWar("zzzzs");    //=> Right side wins!
+//alphabetWar("wwwwwwz");  //=> Left side wins!
+
+function alphabetWar(fight){
+  //each letter is assigned a numeerical value
+  //one set is labeled left the second set is labeled right
+  //the argument will be a string consisting of these letters
+  //if a string contains value that are worth more en eother side that side wins
+  // the goal is declare a winner and if there is no winner aka tie then the retuen message will be lets fight again
+  //left: w-4,p-3, b-3, s-1
+  //right: m-4,q-3,d-2,z-1
+
+  //make  two empty arrys 
+  //run a loop over each letter and assign a number to push to each corresponding array
+
+  
+  function sum(arr){
+   return  arr.reduce((acc,c) => acc+c, 0)
+  }
+
+  
+
+  let left = [];
+  let right = [];
+  const arr = fight.split("")
+  //cl(arr)
+  for(let i=0; i<arr.length;i++){
+    //cl(arr[i])
+    switch(arr[i]){
+      case "w":
+        left.push(4)
+        break;
+      case "p":
+        left.push(3)
+        break;
+      case "b":
+        left.push(2)
+        break;
+      case "s":
+        left.push(1)
+        break;
+
+      case "m":
+        right.push(4)
+        break;
+      case "q":
+        right.push(3)
+        break;
+      case "d":
+        right.push(2)
+        break;
+      case "z":
+        right.push(1)
+        break;
+        default:
+        
+    }
+     
+  }
+  //cl(left)
+  //cl(right)
+  const leftSum = sum(left)
+  const rightSum = sum(right)
+  if(leftSum > rightSum){
+    return "Left side wins!"
+  }else if(rightSum >leftSum){
+    return "Right side wins!"
+  }else return "Let's fight again!";
+   
+}
+
+cl(alphabetWar(""))
+cl(alphabetWar("zdqmwpbs"))
