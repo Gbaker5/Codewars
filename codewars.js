93bd9b835970742eb5f22fdc10ac558425f63f89
Gjarred Baker <gjarred23@gmail.com>
2025-09-17 23:36:35 -0500

    merge two arrays

--- codewars.js
+++ codewars.js
@@ -5694,3 +5694,31 @@ function mango(quantity, price){
 
   return totalmangosPaid * price
 }
+
+//You are given two sorted arrays that contain only integers. These arrays may be sorted in either ascending or descending order. Your task is to merge them into a single array, ensuring that:
+//
+//The resulting array is sorted in ascending order.
+//
+//Any duplicate values are removed, so each integer appears only once.
+//
+//If both input arrays are empty, return an empty array.
+//
+//No input validation is needed, as both arrays are guaranteed to contain zero or more integers.
+//
+//Examples (input -> output)
+//
+//* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+//* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+//* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+function mergeArrays(arr1, arr2) {
+  const whole = arr1.concat(arr2).sort((a,b)=> a-b)
+  //cl(whole)
+  const noDup = whole.filter((item, index) => whole.indexOf(item) == index)
+  //cl(noDup)
+  return noDup
+}
+
+cl(mergeArrays([1,3,5,7,9], [10,8,6,4,2]))
