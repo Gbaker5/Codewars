4f14a4b3c4b475d1272b32d39ab9ced2b95722b2
Gjarred Baker <gjarred23@gmail.com>
2025-09-20 09:58:00 -0500

    sequence sum

--- codewars.js
+++ codewars.js
@@ -5929,3 +5929,57 @@ function rowWeights(array){
 }
 
 rowWeights([50,60,70,80]) //[120,140]
+
+//We want to generate a function that computes the series starting from 0 and ending until the given number.
+//
+//Example:
+//
+//Input:
+//
+//> 6
+//Output:
+//
+//0+1+2+3+4+5+6 = 21
+//
+//Input:
+//
+//> -15
+//Output:
+//
+//-15<0
+//
+//Input:
+//
+//> 0
+//Output:
+//
+//0=0
+
+
+var SequenceSum = (function(count) {
+
+  function SequenceSum() {}
+
+SequenceSum.showSequence = function(count) {
+    let arr = []
+    if(count == 0){
+      return "0=0"
+    }else if(count< 0){
+      return "count<0"
+    }else{
+      for(let i=0;i<count+1;i++){
+       arr.push(i) 
+      }
+      
+    }
+    let sum = arr.reduce((acc,c) => acc +c)
+    const str = arr.join("+")
+    const sumStr = `${str} = ${sum}`
+    cl(sumStr)
+}
+
+  return sumStr;
+
+})();
+
+SequenceSum(6)
