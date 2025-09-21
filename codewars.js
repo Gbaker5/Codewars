67c672283c13834a9b78df4906d2af3e94d23d11
Gjarred Baker <gjarred23@gmail.com>
2025-09-21 16:36:58 -0500

    wordstoMarks

--- codewars.js
+++ codewars.js
@@ -5983,3 +5983,30 @@ SequenceSum.showSequence = function(count) {
 })();
 
 SequenceSum(6)
+
+//If　a = 1, b = 2, c = 3 ... z = 26
+//
+//Then l + o + v + e = 54
+//
+//and f + r + i + e + n + d + s + h + i + p = 108
+//
+//So friendship is twice as strong as love :-)
+//
+//Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+//
+//The input will always be made of only lowercase letters and will never be empty.
+
+function wordsToMarks(string){
+  //your code here
+  
+  let count = 0;
+
+  for(letter of string){
+   
+      count+= letter.charCodeAt(0) - 96;
+    
+  }
+  console.log(count)
+}
+
+wordsToMarks("abc") //6
