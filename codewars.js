b319d6a8cd19d7052f594f4ba45188f6e9774e0e
Gjarred Baker <gjarred23@gmail.com>
2025-12-30 12:52:37 -0600

    sum of two lowest positive integers

--- codewars.js
+++ codewars.js
@@ -6047,3 +6047,20 @@ wordsToMarks("abc") //6
 //
 //let s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 //console.log(meeting(s));
+
+
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+//
+//For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+//
+//[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  // Code here
+  const sorted = numbers.sort((a,b) => a - b)
+  return sorted[0] + sorted[1]
+}
+
+cl(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
+cl(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
