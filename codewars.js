4482d1d510f6dd4e944cbbf71aa0206db275c254
Gjarred Baker <gjarred23@gmail.com>
2025-12-30 14:11:49 -0600

    check exam

--- codewars.js
+++ codewars.js
@@ -6064,3 +6064,43 @@ function sumTwoSmallestNumbers(numbers) {
 
 cl(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
 cl(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
+
+//The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+//
+//The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+//
+//If the score < 0, return 0.
+//
+//For example:
+//
+//    Correct answer    |    Student's answer   |   Result         
+// ---------------------|-----------------------|-----------
+// ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+// ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+// ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+// ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+
+
+function checkExam(array1, array2) {
+ // good luck
+ let score = 0;
+ for(let i=0;i<array1.length;i++){
+  if(array1[i] == array2[i]){
+    score += 4
+  }else if(array1[i] != array2[i] && array2[i] == ""){
+    score += 0
+ }else 
+    score -= 1
+}
+
+cl(score)
+//if (score < 0){
+//  return 0
+//}else return score
+}
+
+
+checkExam(["a", "a", "b", "b"] , ["a", "c", "b", "d"])
+checkExam(["a", "a", "c", "b"] ,  ["a", "a", "b", "" ])
+checkExam(["a", "a", "b", "c"] ,  ["a", "a", "b", "c"])
+checkExam(["b", "c", "b", "a"] , ["" , "a", "a", "c"])
