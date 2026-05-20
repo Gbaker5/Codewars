4edbaffc568e45b7cb9e105425e86da47fee19a9
Gjarred Baker <gjarred23@gmail.com>
2026-05-19 19:28:53 -0500

    isVow

--- codewars.js
+++ codewars.js
@@ -6104,3 +6104,42 @@ checkExam(["a", "a", "b", "b"] , ["a", "c", "b", "d"])
 checkExam(["a", "a", "c", "b"] ,  ["a", "a", "b", "" ])
 checkExam(["a", "a", "b", "c"] ,  ["a", "a", "b", "c"])
 checkExam(["b", "c", "b", "a"] , ["" , "a", "a", "c"])
+
+
+//Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+//
+//If they are, change the array value to a string of that vowel.
+//
+//input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
+
+function isVow(a){
+  //console.log(a)
+
+  
+  let charFilter = []
+
+  for(i=0;i<a.length;i++){
+    if( a[i] == 97 ){
+      charFilter.push(String.fromCharCode(97))
+    }else if(a[i] == 101){
+      charFilter.push(String.fromCharCode(101))
+    }else if(a[i] == 105){
+      charFilter.push(String.fromCharCode(105))
+     }else if(a[i] == 111){
+      charFilter.push(String.fromCharCode(111))
+     }else if(a[i] == 117){
+      charFilter.push(String.fromCharCode(117))
+     }else {
+      charFilter.push(a[i])
+     }
+
+     
+
+  }
+  console.log(charFilter)
+
+}
+
+
+isVow([97,101,105,111,117,5])
+//isVow([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106])
