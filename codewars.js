e73fb7ee483fc94425eb6fde266cfa675a05577b
Gjarred Baker <gjarred23@gmail.com>
2026-05-22 18:45:04 -0500

    shark

--- codewars.js
+++ codewars.js
@@ -6216,3 +6216,59 @@ function smallEnough(a, limit){
  const arr =  a.map((x) => x <= limit ? true : false)
  return arr.includes(false) ? false : true
 }
+
+
+//Your friend invites you out to a cool floating pontoon around 1km off the beach. Among other things, the pontoon has a huge slide that drops you out right into the ocean, a small way from a set of stairs used to climb out.
+//
+//As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+//
+//You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+//
+//You are given 5 variables;
+//
+//sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+//
+//sharkSpeed = how fast it can move in metres/second.
+//
+//pontoonDistance = how far you need to swim to safety in metres.
+//
+//youSpeed = how fast you can swim in metres/second.
+//
+//dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+//
+//The pontoon, you, and the shark are all aligned in one dimension.
+//
+//If you make it, return "Alive!", if not, return "Shark Bait!".
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+//if shark gets to boat before you you die. (JAWS)
+//we are in a straight line . boat - me - shark >=D
+//variabhle given:
+// distance between me and boat (m)
+//speed im swimming (olympian speed) (m/s)
+//distance between shark and boat (m)
+//shark speed (m/s)
+//suprise variable DOLPHIN (immortal enemy of shark - undefeated)
+//if dolphin is present [boolean] speed of shark is halved
+
+
+//boat <- me 12m (4m/s) <- shark 50m (8m/s) - is there a dolphin?
+let meToB = pontoonDistance
+let sharkToB = sharkDistance
+
+let sharkSp = dolphin ? (sharkSpeed * 0.5) : sharkSpeed
+//console.log(sharkSp)
+
+while(meToB < sharkToB && meToB > 0 && sharkToB > 0){
+  meToB = meToB - youSpeed
+  sharkToB = sharkToB - sharkSp
+  console.log("My Distance:" + meToB)
+  console.log("Shark Distance:" + sharkToB)
+}
+
+return meToB < sharkToB ? "Alive!" : "Shark Bait!"
+
+
+}
+
+shark(25,55,0,2, true)
