f29eebabee053884958d9d205b85281c102dd7f5
Gjarred Baker <gjarred23@gmail.com>
2026-05-29 15:54:45 -0500

    fibProduct

--- codewars.js
+++ codewars.js
@@ -6302,3 +6302,101 @@ function sortMyString(S) {
     }
   return evens.join("") + " " + odds.join("")
 }
+
+//The Fibonacci numbers are the numbers in the following integer sequence (Fn): 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+//
+//such that:
+//
+//F
+//(
+//0
+//)
+//=
+//0
+//F
+//(
+//1
+//)
+//=
+//1
+//F
+//(
+//n
+//)
+//=
+//F
+//(
+//n
+//−
+//1
+//)
+//+
+//F
+//(
+//n
+//−
+//2
+//)
+//F(0)=0
+//F(1)=1
+//F(n)=F(n−1)+F(n−2)
+//Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying:
+//
+//F
+//(
+//n
+//)
+//∗
+//F
+//(
+//n
+//+
+//1
+//)
+//=
+//p
+//r
+//o
+//d
+//F(n)∗F(n+1)=prod
+//Your function takes an integer (prod) and returns an array/tuple (check the function signature/sample tests for the return type in your language):
+//
+//if F(n) * F(n+1) = prod:
+//(F(n), F(n+1), true)
+//If you do not find two consecutive F(n) verifying F(n) * F(n+1) = prod:
+//(F(n), F(n+1), false)
+//where F(n) is the smallest one such as F(n) * F(n+1) > prod.
+//Examples:
+//
+//714 ---> (21, 34, true)
+//--> since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+//
+//800 --->  (34, 55, false)
+//--> since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+//
+
+
+function productFib(prod){
+ 
+      let arr = [1,1];
+      let currentProduct = 0;
+      let sum = 0
+    //perform sequence until number is greater than product
+  
+   while(currentProduct < prod){
+    
+    sum = arr[0] + arr[1] //2 
+
+    arr.splice(0,1)
+    arr.push(sum)
+
+    currentProduct = arr[0] * arr[1]
+    
+    }
+  
+   if(prod == currentProduct){
+     return [arr[0], arr[1], true]
+   } else return [arr[0], arr[1], false]
+}
+
+console.log(productFib(4895))
