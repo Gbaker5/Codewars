0d89a53c98ae035db86eccf770f1e80f6d555d30
Gjarred Baker <gjarred23@gmail.com>
2026-05-29 17:19:46 -0500

    all

--- codewars.js
+++ codewars.js
@@ -6400,3 +6400,32 @@ function productFib(prod){
 }
 
 console.log(productFib(4895))
+
+
+//Task
+//
+//Create a method all which takes two params:
+//
+//a sequence
+//a function
+//and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+//
+//Example
+//
+//all((1, 2, 3, 4, 5), greater_than_9) -> false
+//all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+function all(arr, fun) {
+  let check = 0;
+
+  for(i=0;i<arr.length;i++){
+    if(fun(arr[i])){
+      check += 1
+    }
+  }
+
+  return check == arr.length ? true : false
+    
+}
+
+all((1, 2, 3, 4, 5))
