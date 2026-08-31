737a3b59239db2a0f5aedce801145075f60e6e7f
Gjarred Baker <gjarred23@gmail.com>
2026-08-30 19:26:28 -0500

    sum of angles

--- codewars.js
+++ codewars.js
@@ -6682,3 +6682,10 @@ function flattenAndSort(array) {
   return array.flat(Infinity)
         .sort((a,b) => a-b)
 }
+
+
+//Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+function angle(n) {
+  return (n-2)* 180
+}
